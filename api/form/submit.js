565790c91c2336d05d9bdbba645961f1b93613a8
william8815 <williamhsu88157976@gmail.com>
2026-01-15 17:20:33 +0800

    fix(vercel) fix api file config problem

--- api/form/submit.js
+++ api/form/submit.js
@@ -1,7 +1,3 @@
-export const config = {
-  runtime: "nodejs18.x",
-};
-
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method Not Allowed" });
